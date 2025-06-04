2c6a1ad9fabcb87c048eb3860ed4d34e2033425d
Jeremy <jeffcode.easy@gmail.com>
2025-06-04 12:21:57 -0500

    add header waves

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -184,3 +184,52 @@ class _HeaderCurvoPainter extends CustomPainter {
     return true;
   }
 }
+
+class HeaderWaves extends StatelessWidget {
+  const HeaderWaves({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(painter: _HeaderWavesPainter()),
+    );
+  }
+}
+
+class _HeaderWavesPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final lapiz = Paint();
+    final path = Path();
+    // Propiedades
+    lapiz.color = Color(0xff615AAB);
+    lapiz.strokeWidth = 20;
+    lapiz.style = PaintingStyle.fill;
+
+    // Dibujar con el path y el lapiz
+    path.lineTo(0, size.height * 0.25);
+
+    path.quadraticBezierTo(
+      size.width * 0.25,
+      size.height * 0.30,
+      size.width * 0.50,
+      size.height * 0.25,
+    );
+    path.quadraticBezierTo(
+      size.width * 0.75,
+      size.height * 0.20,
+      size.width,
+      size.height * 0.25,
+    );
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, lapiz);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+}
