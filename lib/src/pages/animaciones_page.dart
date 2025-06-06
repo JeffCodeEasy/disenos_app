d4d13aa85d5962207a127cdd30ae0e29fb713a25
Jeremy <jeffcode.easy@gmail.com>
2025-06-05 22:36:53 -0500

    mover cuadrado transform.translate

--- lib/src/pages/animaciones_page.dart
+++ lib/src/pages/animaciones_page.dart
@@ -23,6 +23,7 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
   late AnimationController controller;
   late Animation<double> rotacion;
   late Animation<double> opacidad;
+  late Animation<double> moverDerecha;
 
   @override
   void initState() {
@@ -38,7 +39,14 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
       end: 2 * math.pi,
     ).animate(CurvedAnimation(parent: controller, curve: Curves.easeOut));
 
-    opacidad = Tween(begin: 0.1, end: 1.0).animate(controller);
+    opacidad = Tween(begin: 0.1, end: 1.0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0, 0.25, curve: Curves.easeOut),
+      ),
+    );
+
+    moverDerecha = Tween(begin: 0.0, end: 200.0).animate(CurvedAnimation(parent: controller, curve: Curves.easeOut,));
 
     controller.addListener(() {
       print('Status: ${controller.status}');
@@ -66,11 +74,11 @@ class _CuadradoAnimadoState extends State<CuadradoAnimado>
       animation: controller,
       child: _Rectangulo(),
       builder: (BuildContext context, Widget? childRectangulo) {
-        return Transform.rotate(
-          angle: rotacion.value,
-          child: Opacity(
-            opacity: opacidad.value,
-            child: childRectangulo,
+        return Transform.translate(
+          offset: Offset(moverDerecha.value, 0),
+          child: Transform.rotate(
+            angle: rotacion.value,
+            child: Opacity(opacity: opacidad.value, child: childRectangulo),
           ),
         );
       },
